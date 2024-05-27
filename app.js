21a2b14dae2b2a98d7c4b84b8387162406827936
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-05-28 01:46:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
-let btn6 = document.getElementById("btn7");
-let btn6 = document.getElementById("btn8");
-let btn6 = document.getElementById("btn9");
-let btn6 = document.getElementById("btn10");
+let btn7 = document.getElementById("btn7");
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn10 = document.getElementById("btn10");
 
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
