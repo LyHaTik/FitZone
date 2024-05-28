f2c024e4e6967709f2c052f05fa6d145706d2ad9
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-05-28 22:51:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+function toNum(str) {
+  const num = Number(str.replace(/ /g, ""));
+  return num;
+}
+
+function toCurrency(num) {
+  const format = new Intl.NumberFormat("ru-RU", {
+    style: "currency",
+    currency: "RUB",
+    minimumFractionDigits: 0,
+  }).format(num);
+  return format;
+}
+
 let tg = window.Telegram.WebApp;
 
 tg.expand();
