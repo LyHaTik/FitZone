c143854a7dcec40d9f5cee30ca63004938a56a8e
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-05-28 22:55:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,84 @@ function toCurrency(num) {
   return format;
 }
 
+const cardAddArr = Array.from(document.querySelectorAll(".card__add"));
+const cartNum = document.querySelector("#cart_num");
+const cart = document.querySelector("#cart");
+
+const popup = document.querySelector(".popup");
+const popupClose = document.querySelector("#popup_close");
+const body = document.body;
+const popupContainer = document.querySelector("#popup_container");
+const popupProductList = document.querySelector("#popup_product_list");
+const popupCost = document.querySelector("#popup_cost");
+const popupDiscount = document.querySelector("#popup_discount");
+const popupCostDiscount = document.querySelector("#popup_cost_discount");
+
+cart.addEventListener("click", (e) => {
+  e.preventDefault();
+  popup.classList.add("popup--open");
+  body.classList.add("lock");
+});
+
+popupClose.addEventListener("click", (e) => {
+  e.preventDefault();
+  popup.classList.remove("popup--open");
+  body.classList.remove("lock");
+});
+
+class Product {
+  imageSrc;
+  name;
+  price;
+  priceDiscount;
+  constructor(card) {
+    this.imageSrc = card.querySelector(".card__image").children[0].src;
+    this.name = card.querySelector(".card__title").innerText;
+    this.price = card.querySelector(".card__price--common").innerText;
+    this.priceDiscount = card.querySelector(".card__price--discount").innerText;
+  }
+}
+
+const cardAddArr = Array.from(document.querySelectorAll(".card__add"));
+const cartNum = document.querySelector("#cart_num");
+
+class Cart {
+  products;
+  constructor() {
+    this.products = [];
+  }
+  get count() {
+    return this.products.length;
+  }
+  addProduct(product) {
+    this.products.push(product);
+  }
+  removeProduct(index) {
+    this.products.splice(index, 1);
+  }
+  get cost() {
+    const prices = this.products.map((product) => {
+      return toNum(product.price);
+    });
+    const sum = prices.reduce((acc, num) => {
+      return acc + num;
+    }, 0);
+    return sum;
+  }
+  get costDiscount() {
+    const prices = this.products.map((product) => {
+      return toNum(product.priceDiscount);
+    });
+    const sum = prices.reduce((acc, num) => {
+      return acc + num;
+    }, 0);
+    return sum;
+  }
+  get discount() {
+    return this.cost - this.costDiscount;
+  }
+}
+
 let tg = window.Telegram.WebApp;
 
 tg.expand();
