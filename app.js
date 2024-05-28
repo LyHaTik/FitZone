7ccfa2826dbfcab568e25643fd3c2da51401c5e7
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-05-28 22:58:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -90,7 +90,84 @@ class Cart {
   }
 }
 
+const myCart = new Cart();
+
+if (localStorage.getItem("cart") == null) {
+  localStorage.setItem("cart", JSON.stringify(myCart));
+}
+
+const savedCart = JSON.parse(localStorage.getItem("cart"));
+myCart.products = savedCart.products;
+cartNum.textContent = myCart.count;
+
 let tg = window.Telegram.WebApp;
+
+myCart.products = cardAddArr.forEach((cardAdd) => {
+  cardAdd.addEventListener("click", (e) => {
+    e.preventDefault();
+    const card = e.target.closest(".card");
+    const product = new Product(card);
+    const savedCart = JSON.parse(localStorage.getItem("cart"));
+    myCart.products = savedCart.products;
+    myCart.addProduct(product);
+    localStorage.setItem("cart", JSON.stringify(myCart));
+    cartNum.textContent = myCart.count;
+  });
+});
+
+function popupContainerFill() {
+  popupProductList.innerHTML = null;
+  const savedCart = JSON.parse(localStorage.getItem("cart"));
+  myCart.products = savedCart.products;
+  const productsHTML = myCart.products.map((product) => {
+    const productItem = document.createElement("div");
+    productItem.classList.add("popup__product");
+
+    const productWrap1 = document.createElement("div");
+    productWrap1.classList.add("popup__product-wrap");
+    const productWrap2 = document.createElement("div");
+    productWrap2.classList.add("popup__product-wrap");
+
+    const productImage = document.createElement("img");
+    productImage.classList.add("popup__product-image");
+    productImage.setAttribute("src", product.imageSrc);
+
+    const productTitle = document.createElement("h2");
+    productTitle.classList.add("popup__product-title");
+    productTitle.innerHTML = product.name;
+
+    const productPrice = document.createElement("div");
+    productPrice.classList.add("popup__product-price");
+    productPrice.innerHTML = toCurrency(toNum(product.priceDiscount));
+
+    const productDelete = document.createElement("button");
+    productDelete.classList.add("popup__product-delete");
+    productDelete.innerHTML = "✖";
+
+    productDelete.addEventListener("click", () => {
+      myCart.removeProduct(product);
+      localStorage.setItem("cart", JSON.stringify(myCart));
+      popupContainerFill();
+    });
+
+    productWrap1.appendChild(productImage);
+    productWrap1.appendChild(productTitle);
+    productWrap2.appendChild(productPrice);
+    productWrap2.appendChild(productDelete);
+    productItem.appendChild(productWrap1);
+    productItem.appendChild(productWrap2);
+
+    return productItem;
+  });
+
+  productsHTML.forEach((productHTML) => {
+    popupProductList.appendChild(productHTML);
+  });
+
+  popupCost.value = toCurrency(myCart.cost);
+  popupDiscount.value = toCurrency(myCart.discount);
+  popupCostDiscount.value = toCurrency(myCart.costDiscount);
+}
 
 tg.expand();
 
