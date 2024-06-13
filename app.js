6a1d0a336c0306e0dc236a2a08f673e47571a805
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-06-13 17:21:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
-let btn7 = document.getElementById("btn7");
-let btn8 = document.getElementById("btn8");
-let btn9 = document.getElementById("btn9");
-let btn10 = document.getElementById("btn10");
 
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
@@ -84,49 +80,6 @@ btn6.addEventListener("click", function(){
 	}
 });
 
-btn7.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали Рашгард!");
-		item = "7";
-		tg.MainButton.show();
-	}
-});
-
-btn8.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали Тестобустер!");
-		item = "8";
-		tg.MainButton.show();
-	}
-});
-
-btn9.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали Топ!");
-		item = "9";
-		tg.MainButton.show();
-	}
-});
-
-btn10.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали Ветровка!");
-		item = "10";
-		tg.MainButton.show();
-	}
-});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
