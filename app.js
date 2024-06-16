22f6109627064e0373cc639341ef70db676271c9
LyHaTik <96049450+LyHaTik@users.noreply.github.com>
2024-06-16 20:38:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 
 let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
 let order = document.getElementById("order");
 
 btn1.addEventListener("click", function(){
